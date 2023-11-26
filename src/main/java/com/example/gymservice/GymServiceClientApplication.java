59c6a40d8d40223a80bcf43afc467047c870092a
adewaleijalana <adewaleijalana@gmail.com>
2023-11-26 09:48:46 +0100

    first commit

---
+++ src/main/java/com/example/gymservice/GymServiceClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymServiceClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymServiceClientApplication.class, args);
+    }
+
+}
